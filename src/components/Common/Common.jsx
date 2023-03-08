29467cf5f21aea83e74e88b120260cf2444a5265
Bektursun Baimamatov <bektursunbaimamatov@MacBook-Pro-Bektursun.local>
2023-03-08 12:29:48 +0600

    add common title

--- src/components/Common/Common.jsx
+++ src/components/Common/Common.jsx
@@ -4,13 +4,14 @@ import "./common.scss"
 const Common = () => {
     return (
         <setion className="common">
+            <img className="common__background" src="../static/img/architecture-bg.png" alt="architecture-bg"/>
             <div className="common__container">
                 <div className="common__wrapper">
-                    <div className="common__inner">
-                        <div className="common__content">
+                    <h2 className="common__title">ДВУХ ЭТАЖНЫЙ ДОМ СОВРЕМЕННОГО СТИЛЯ</h2>
+                    <p className="common__subtitle">300 МЕТР КВ</p>
+                </div>
+                <div className="common__arrow">
 
-                        </div>
-                    </div>
                 </div>
             </div>
         </setion>
