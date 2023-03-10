c75960e75b1f3ba89fc03a845c387b974727e8e2
Ulukbek Tynchtykbekov <ulukbektynctykbekov@gmail.com>
2023-03-10 07:24:59 -0800

    correct some conflicts

--- src/layout/Header/Header.jsx
+++ src/layout/Header/Header.jsx
@@ -9,41 +9,41 @@ const Header = () => {
     const {bgParallaxStyle} = useParallax()
 
     return (
-      <header className="header" style={bgParallaxStyle}>
-          <div className="container">
-             <div className="header-in">
-                 <div className="logo">
-                     <img className="logo__img" src={Logo} alt=""/>
-                 </div>
-                 <nav className={`menu ${isOpen && "open"}`}>
-                     <ul className="menu__item">
-                         <li className="menu__link">
-                             <Link className="menu__title" to="/projects">ПРОЕКТЫ</Link>
-                         </li>
-                         <li className="menu__link">
-                             <Link className="menu__title" to="/services">УСЛУГИ</Link>
-                         </li>
-                         <li className="menu__link">
-                             <Link className="menu__title" to="/consultation">КОНСУЛЬТАЦИЯ</Link>
-                         </li>
-                         <li className="menu__link">
-                             <Link className="menu__title" to="/about">О НАС</Link>
-                         </li>
-                     </ul>
-                     <button className="menu__btn">
-                         <Link className="btn_link" to="/contacts">КОНТАКТЫ</Link>
-                     </button>
-                 </nav>
-                 <div className={`nav-toggle ${isOpen && "open"}`} onClick={()=> setIsOpen(!isOpen)
-                 }>
-                     <div className="bar">
+        <header className="header" style={bgParallaxStyle}>
+            <div className="container">
+                <div className="navigation">
+                    <div className="logo">
+                        <img className="logo__img" src={Logo} alt=""/>
+                    </div>
+                    <nav className={`menu ${isOpen && "open"}`}>
+                        <ul className="menu__item">
+                            <li className="menu__link">
+                                <Link className="menu__title" to="/projects">ПРОЕКТЫ</Link>
+                            </li>
+                            <li className="menu__link">
+                                <Link className="menu__title" to="/services">УСЛУГИ</Link>
+                            </li>
+                            <li className="menu__link">
+                                <Link className="menu__title" to="/consultation">КОНСУЛЬТАЦИЯ</Link>
+                            </li>
+                            <li className="menu__link">
+                                <Link className="menu__title" to="/about">О НАС</Link>
+                            </li>
+                            <li className="menu__button button">
+                                <Link className="menu__btn" to="/contacts">КОНТАКТЫ</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div className={`nav-toggle ${isOpen && "open"}`} onClick={() => setIsOpen(!isOpen)
+                    }>
+                        <div className="bar">
 
-                     </div>
-                 </div>
-             </div>
-          </div>
-          <Outlet />
-      </header>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Outlet/>
+        </header>
     );
 };
 
